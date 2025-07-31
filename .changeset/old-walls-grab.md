c84e0b2139de3278f7e73c41e91ce2dde3c05bd7
drashti <djborad0890@gmail.com>
2025-07-31 07:44:12 +0530

    add dropdown and minor change in ci

---
+++ .changeset/old-walls-grab.md
@@ -0,0 +1,5 @@
+---
+"react-uni-component": minor
+---
+
+update dropdown component and ci
--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,4 @@
 import "./index.css";
 export { default as Button } from "./components/button";
 export { default as Slider } from "./components/carousel";
+export { default as Dropdown } from "./components/dropdown";
